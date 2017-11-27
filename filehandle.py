bf7b395a9249a8a5f8f8067e1ad0bcc9895a09a4
Muyierning <34019977+Muyierning@users.noreply.github.com>
2017-11-27 14:48:05 +0800

    my python

---
+++ filehandle.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/python3
+import os, sys
+retval = os.getcwd()
+print ("当前工作目录为 %s" % retval)
+os.chdir("D:\myPython")
+# 查看修改后的工作目录
+retval = os.getcwd()
+print ("目录修改成功 %s" % retval)
+with open('test.txt','r') as f1:
+    with open('f2.txt','w') as f2:
+        while True:  
+            line = f1.readline()
+            if not line:  
+                break
+            if( "歌" not in line) :		
+                f2.write(line)
